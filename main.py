b37de554a56c787929720d9d629ff378836d4a15
Swt-troublemaker <125016217+Swt-troublemaker@users.noreply.github.com>
2023-02-20 16:00:52 +0530

    Love Calculator
    You are going to write a program that tests the compatibility between two people.
    
    To work out the love score between two people:
    
    Take both people's names and check for the number of times the letters in the word TRUE occurs. 
    
    Then check for the number of times the letters in the word LOVE occurs. 
    
    Then combine these numbers to make a 2 digit number.
    
    For Love Scores less than 10 or greater than 90, the message should be:
    
    "Your score is **x**, you go together like coke and mentos."
    For Love Scores between 40 and 50, the message should be:
    
    "Your score is **y**, you are alright together."
    Otherwise, the message will just be their score. e.g.:
    
    "Your score is **z**."
    e.g.
    
    name1 = "Angela Yu"
    name2 = "Jack Bauer"
    T occurs 0 times
    
    R occurs 1 time
    
    U occurs 2 times
    
    E occurs 2 times
    
    Total = 5
    
    L occurs 1 time
    
    O occurs 0 times
    
    V occurs 0 times
    
    E occurs 2 times
    
    Total = 3
    
    Love Score = 53
    
    Print: "Your score is 53."
    
    Example Input 1
    name1 = "Kanye West"
    name2 = "Kim Kardashian"
    Example Output 1
    Your score is 42, you are alright together.
    Example Input 2
    name1 = "Brad Pitt"
    name2 = "Jennifer Aniston"
    Example Output 2
    Your score is 73.

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
-# START
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 
-# PRICES
-spc = 18
-mpc = 24
-lpc = 29
-sp = 17
-mp = 23
-lp = 28
-sc = 16
-mc = 21
-lc = 26
-s = 15
-m = 20
-l = 25
-if (size == "S") and (add_pepperoni == "Y") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${spc}.")
-elif (size == "S") and (add_pepperoni == "Y") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${sp}.")
-elif (size == "S") and (add_pepperoni == "N") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${sc}.")
-elif (size == "S") and (add_pepperoni == "N") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${s}.")
-elif (size == "M") and (add_pepperoni == "Y") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${mpc}.")
-elif (size == "M") and (add_pepperoni == "Y") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${mp}.")
-elif (size == "M") and (add_pepperoni == "N") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${mc}.")
-elif (size == "M") and (add_pepperoni == "N") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${m}.")
-elif (size == "L") and (add_pepperoni == "Y") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${lpc}.")
-elif (size == "L") and (add_pepperoni == "Y") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${lp}.")
-elif (size == "L") and (add_pepperoni == "N") and (extra_cheese == "Y"):
-    print(f"Your final bill is: ${lc}.")
-elif (size == "L") and (add_pepperoni == "N") and (extra_cheese == "N"):
-    print(f"Your final bill is: ${l}.")
+name = name1 + name2
+name3 = name.lower()
+t = int(name3.count('t'))
+r = int(name3.count('r'))
+u = int(name3.count('u'))
+e = int(name3.count('e'))
+l = int(name3.count('l'))
+o = int(name3.count('o'))
+v = int(name3.count('v'))
+e = int(name3.count('e'))
+true = t + r + u + e
+love = l + o + v + e
+love_score = int(str(true) + str(love))
+if (love_score < 10) or (90 < love_score):
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif 40 <= love_score <= 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")

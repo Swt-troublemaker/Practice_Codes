2f5b7688fdb45c184e63a34d9ba55cc4b95aae19
Swt-troublemaker <125016217+Swt-troublemaker@users.noreply.github.com>
2023-02-20 16:39:49 +0530

    Blind Auction

--- main.py
+++ main.py
@@ -1,50 +1,31 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text = ""
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for char in start_text:
-        # TODO-3: What happens if the user enters a number/symbol/space?
-        # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-        # e.g. start_text = "meet me at 3"
-        # end_text = "•••• •• •• 3"
-        if char in alphabet:
-            position = alphabet.index(char)
-            new_position = position + shift_amount
-            end_text += alphabet[new_position]
-        else:
-            end_text += char
-    print(f"Here's the {cipher_direction}d result: {end_text}")
-
-
-# TODO-1: Import and print the logo from art.py when the program starts.
+from replit import clear
 from art import logo
 
 print(logo)
 
-# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-should_end = False
-while not should_end:
+bids = {}
+bidding_finished = False
 
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    # TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-    # Try running the program and entering a shift number of 45.
-    # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-    # Hint: Think about how you can use the modulus (%).
-    shift = shift % 26
 
-    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    # bidding_record = {"Angela": 123, "James": 321}
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
-    if restart == "no":
-        should_end = True
-        print("Goodbye")
+
+while not bidding_finished:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
